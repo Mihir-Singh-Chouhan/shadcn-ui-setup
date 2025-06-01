1c0d7438a3e93f967209895f7d834ab465365150
Mihir-Singh-Chouhan <mschouhan6855@gmail.com>
2025-06-02 00:52:15 +0530

    Intial Setup

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import {Login} from './page/auth/Login'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    {/* <App /> */}
+    <Login/>
+  </StrictMode>,
+)
